9cde15402511b0974e97be20ab1fe3f85fbfa560
Corey W <whitlowcorey30@gmail.com>
2022-01-11 23:41:37 -0500

    Created db context, setup database, created MigrationManager, and ran initial migration

---
+++ api/Data/ApiContext.cs
@@ -0,0 +1,27 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Jake Busey",
+                    Description = "Gary Busey's dangerously insane brother",
+                    Price = 1m,
+                    rating = 0
+                });
+        }
+    }
+}
---
+++ api/MigrationManager.cs
@@ -0,0 +1,30 @@
+﻿using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace api
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<ApiContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                        Console.WriteLine("Migrations complete");
+                    }
+                    catch (Exception ex)
+                    {
+                        // eventually log this error
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
