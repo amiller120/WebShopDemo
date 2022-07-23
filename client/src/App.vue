0ecf702e012206de65c11b33491e0c691d9cbd2b
amiller <adam5bucks475@gmail.com>
2022-07-23 16:07:12 -0400

    Add vue router with shop page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<Navbar></Navbar>
+  <router-view/>
 </template>
 
 <script setup lang="ts">
+import Navbar from './components/Navbar.vue';
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ client/src/Views/Shop.vue
@@ -0,0 +1,12 @@
+<template>
+    <Product></Product>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Product from '../components/Product.vue'
+
+export default{
+    components: {Product}
+}
+</script>
